6c390f35f2a3274f8ed7355e513a7a0b65a31699
Anselm Binninger <anselm@bwk-technik.de>
2016-02-06 16:55:46 +0100

    Initial commit

---
+++ src/entwicklertage/session2/git/Test.java
@@ -0,0 +1,21 @@
+package entwicklertage.session2.git;
+
+public class Test {
+	
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+}
